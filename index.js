e1014882db54f59e2bf6fee31660d91f21a4be87
Titus Wormer <tituswormer@gmail.com>
2021-04-23 21:07:59 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-// Get the text content of a node.
-// Prefer the node’s plain-text fields, otherwise serialize its children,
-// and if the given value is an array, serialize the nodes in it.
+/**
+ * Get the text content of a node.
+ * Prefer the node’s plain-text fields, otherwise serialize its children,
+ * and if the given value is an array, serialize the nodes in it.
+ *
+ * @param {unknown} node
+ * @returns {string}
+ */
 export function toString(node) {
   return (
     (node &&
+      typeof node === 'object' &&
+      // @ts-ignore looks like a literal.
       (node.value ||
+        // @ts-ignore looks like an image.
         node.alt ||
+        // @ts-ignore looks like an image/link.
         node.title ||
+        // @ts-ignore looks like a parent.
         ('children' in node && all(node.children)) ||
-        ('length' in node && all(node)))) ||
+        (Array.isArray(node) && all(node)))) ||
     ''
   )
 }
 
+/**
+ * @param {Array.<unknown>} values
+ * @returns {string}
+ */
 function all(values) {
+  /** @type {Array.<string>} */
   var result = []
   var index = -1
 
--- types/test.ts
+++
@@ -1,5 +0,0 @@
-import * as mdastUtilToString from 'mdast-util-to-string'
-
-mdastUtilToString() // $ExpectError
-mdastUtilToString({type: 'root'}) // $ExpectType string
-mdastUtilToString([{type: 'root'}]) // $ExpectType string
